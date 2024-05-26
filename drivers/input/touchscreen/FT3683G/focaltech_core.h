acdbdd480a2f7bb226787410b5423e13cd6e9f2e
Elias Dimopoulos <linkdevel@protonmail.com>
2024-05-26 03:21:50 +0300

    DIFF wit nothing
    Signed-off-by: Elias Dimopoulos <linkdevel@protonmail.com>

--- drivers/input/touchscreen/FT3683G/focaltech_core.h
+++ drivers/input/touchscreen/FT3683G/focaltech_core.h
@@ -68,61 +68,70 @@
 /*****************************************************************************
 * Private constant and macro definitions using #define
 *****************************************************************************/
-#define FTS_DRIVER_NAME                     "fts_ts"
+#define FTS_DRIVER_NAME				"fts_ts"
 
-#define FTS_MAX_POINTS_SUPPORT              10 /* constant value, can't be changed */
-#define FTS_MAX_KEYS                        4
-#define FTS_KEY_DIM                         10
-#define FTS_COORDS_ARR_SIZE                 4
-#define FTS_ONE_TCH_LEN                     6
-#define FTS_ONE_TCH_LEN_V2                  8
+#define FTS_MAX_POINTS_SUPPORT			10 /* constant value, can't be changed */
+#define FTS_MAX_KEYS				4
+#define FTS_KEY_DIM				10
+#define FTS_COORDS_ARR_SIZE			4
+#define FTS_ONE_TCH_LEN				6
+#define FTS_ONE_TCH_LEN_V2			8
 #define FTS_TOUCH_DATA_LEN_V2  (FTS_MAX_POINTS_SUPPORT * FTS_ONE_TCH_LEN_V2 + 4)
 
 
 #define FTS_TOUCH_DATA_LEN  (FTS_MAX_POINTS_SUPPORT * FTS_ONE_TCH_LEN + 2)
 
-#define FTS_GESTURE_POINTS_MAX              6
-#define FTS_GESTURE_DATA_LEN               (FTS_GESTURE_POINTS_MAX * 4 + 4)
+#define FTS_GESTURE_POINTS_MAX			6
+#define FTS_GESTURE_DATA_LEN			(FTS_GESTURE_POINTS_MAX * 4 + 4)
 
-#define FTS_SIZE_PEN                        15
-#define FTS_SIZE_DEFAULT                    15
-#define FTS_SIZE_DEFAULT_V2                 21
+#define FTS_SIZE_PEN				15
+#define FTS_SIZE_DEFAULT			15
+#define FTS_SIZE_DEFAULT_V2			21
 
 
-#define FTS_MAX_ID                          0x0A
-#define FTS_TOUCH_OFF_E_XH                  0
-#define FTS_TOUCH_OFF_XL                    1
-#define FTS_TOUCH_OFF_ID_YH                 2
-#define FTS_TOUCH_OFF_YL                    3
-#define FTS_TOUCH_OFF_PRE                   4
-#define FTS_TOUCH_OFF_AREA                  5
-#define FTS_TOUCH_OFF_MINOR                 6
+#define FTS_MAX_ID				0x0A
+#define FTS_TOUCH_OFF_E_XH			0
+#define FTS_TOUCH_OFF_XL			1
+#define FTS_TOUCH_OFF_ID_YH			2
+#define FTS_TOUCH_OFF_YL			3
+#define FTS_TOUCH_OFF_PRE			4
+#define FTS_TOUCH_OFF_AREA			5
+#define FTS_TOUCH_OFF_MINOR			6
 
-#define FTS_TOUCH_E_NUM                     1
-#define FTS_X_MIN_DISPLAY_DEFAULT           0
-#define FTS_Y_MIN_DISPLAY_DEFAULT           0
-#define FTS_X_MAX_DISPLAY_DEFAULT           (720 - 1)
-#define FTS_Y_MAX_DISPLAY_DEFAULT           (1280 - 1)
+#define FTS_TOUCH_E_NUM				1
+#define FTS_X_MIN_DISPLAY_DEFAULT		0
+#define FTS_Y_MIN_DISPLAY_DEFAULT		0
+#define FTS_X_MAX_DISPLAY_DEFAULT		(720 - 1)
+#define FTS_Y_MAX_DISPLAY_DEFAULT		(1280 - 1)
 
-#define FTS_TOUCH_DOWN                      0
-#define FTS_TOUCH_UP                        1
-#define FTS_TOUCH_CONTACT                   2
-#define EVENT_DOWN(flag)                    ((FTS_TOUCH_DOWN == flag) || (FTS_TOUCH_CONTACT == flag))
-#define EVENT_UP(flag)                      (FTS_TOUCH_UP == flag)
+#define FTS_TOUCH_DOWN				0
+#define FTS_TOUCH_UP				1
+#define FTS_TOUCH_CONTACT			2
+#define EVENT_DOWN(flag)			((FTS_TOUCH_DOWN == flag) || (FTS_TOUCH_CONTACT == flag))
+#define EVENT_UP(flag)				(FTS_TOUCH_UP == flag)
 
-#define FTS_MAX_COMPATIBLE_TYPE             8
-#define FTS_MAX_COMMMAND_LENGTH             16
+#define FTS_MAX_COMPATIBLE_TYPE			8
+#define FTS_MAX_COMMMAND_LENGTH			16
 
-#define FTS_MAX_TOUCH_BUF                   4096
-#define FTS_MAX_BUS_BUF                     4096
+#define FTS_MAX_TOUCH_BUF			4096
+#define FTS_MAX_BUS_BUF				4096
 
-/* N17 code for HQ-299546 by liunianliang at 2023/6/13 start */
-#define EXPERT_ARRAY_SIZE                   3
-#define PANEL_ORIENTATION_DEGREE_0          0   /* normal portrait orientation */
-#define PANEL_ORIENTATION_DEGREE_90         1   /* anticlockwise 90 degrees */
-#define PANEL_ORIENTATION_DEGREE_180        2   /* anticlockwise 180 degrees */
-#define PANEL_ORIENTATION_DEGREE_270        3   /* anticlockwise 270 degrees */
-/* N17 code for HQ-299546 by liunianliang at 2023/6/13 end */
+#define FTS_REG_FOD_INFO			0xE1
+#define FTS_REG_FOD_INFO_LEN			9
+#define FTS_REG_FOD_INFO_ID			0x26
+
+#define FTS_TX_MAX 17
+#define FTS_RX_MAX 38
+#define FTS_DIFF_COUNT_LEN 1
+#define FTS_DIFF_DATA_LEN (FTS_TX_MAX*FTS_RX_MAX + (FTS_TX_MAX + FTS_RX_MAX)*2 + 11*2)*2
+/*
+ * start cmd: 0x01
+ * touch data : 62 Bytes
+ * Black Screen Gesture/Edge Suppression Feature Data: 28 Bytes
+ * fast differ: diff_count + Rx*Tx*2 + Tx*2 + Rx*2 + unused*2 + Tx*2 + (Rx+2)*2  + unused*2
+ *                      1  + 38*17*2 + 17*2 + 38*2 +    11*2  + 17*2 +   38*2    + 11*2  = 1557 Bytes
+ */
+#define FTS_DATA_WITH_DIFF_LEN (FTS_TOUCH_DATA_LEN + FTS_GESTURE_DATA_LEN + FTS_DIFF_COUNT_LEN + FTS_DIFF_DATA_LEN)
 
 /*****************************************************************************
 *  Alternative mode (When something goes wrong, the modules may be able to solve the problem.)
@@ -130,10 +139,8 @@
 /*
  * For commnication error in PM(deep sleep) state
  */
-#define FTS_PATCH_COMERR_PM                 0
-#define FTS_TIMEOUT_COMERR_PM               700
-
-#define FTS_TP_ADD 1
+#define FTS_PATCH_COMERR_PM			1
+#define FTS_TIMEOUT_COMERR_PM			700
 
 /*
  * For high resolution
@@ -143,14 +150,14 @@
  * FTS_XXX_HIRES_X, a multiple relative to the original resolution
  * FTS_HI_RES_X_MAX, const value, can't be modified
  */
-#define FTS_TOUCH_HIRES_EN                  0
-#define FTS_TOUCH_HIRES_X                   10
+#define FTS_TOUCH_HIRES_EN			0
+#define FTS_TOUCH_HIRES_X			10
 
-#define FTS_PEN_HIRES_EN                    1
-#define FTS_PEN_HIRES_X                     10
+#define FTS_PEN_HIRES_EN			1
+#define FTS_PEN_HIRES_X				10
 
 
-#define FTS_HI_RES_X_MAX                    16
+#define FTS_HI_RES_X_MAX			16
 
 
 
@@ -159,217 +166,173 @@
 * Private enumerations, structures and unions using typedef
 *****************************************************************************/
 struct ftxxxx_proc {
-    struct proc_dir_entry *proc_entry;
-    u8 opmode;
-    u8 cmd_len;
-    u8 cmd[FTS_MAX_COMMMAND_LENGTH];
+	struct proc_dir_entry *proc_entry;
+	u8 opmode;
+	u8 cmd_len;
+	u8 cmd[FTS_MAX_COMMMAND_LENGTH];
 };
 
 struct fts_ts_platform_data {
-    u32 irq_gpio;
-    u32 irq_gpio_flags;
-    u32 reset_gpio;
-    u32 reset_gpio_flags;
-    u32 iovdd_gpio;
-    u32 iovdd_gpio_flags;
-    bool have_key;
-    u32 key_number;
-    u32 keys[FTS_MAX_KEYS];
-    u32 key_y_coords[FTS_MAX_KEYS];
-    u32 key_x_coords[FTS_MAX_KEYS];
-    char avdd_name[32];
-    char iovdd_name[32];
-    u32 x_max;
-    u32 y_max;
-    u32 x_min;
-    u32 y_min;
-    u32 max_touch_number;
-
-    /* N17 code for HQ-299546 by liunianliang at 2023/6/13 start */
-    u32 touch_range_array[5];
-    u32 touch_def_array[4];
-    u32 touch_expert_array[4 * EXPERT_ARRAY_SIZE];
-    /* N17 code for HQ-299546 by liunianliang at 2023/6/13 end */
+	u32 irq_gpio;
+	u32 irq_gpio_flags;
+	u32 reset_gpio;
+	u32 reset_gpio_flags;
+	bool have_key;
+	u32 key_number;
+	u32 keys[FTS_MAX_KEYS];
+	u32 key_y_coords[FTS_MAX_KEYS];
+	u32 key_x_coords[FTS_MAX_KEYS];
+	u32 x_max;
+	u32 y_max;
+	u32 x_min;
+	u32 y_min;
+	u32 max_touch_number;
 };
 
 struct ts_event {
-    int x;      /*x coordinate */
-    int y;      /*y coordinate */
-    int p;      /* pressure */
-    int flag;   /* touch event flag: 0 -- down; 1-- up; 2 -- contact */
-    int id;     /*touch ID */
-    int area;
-    int minor;
+	int x; /*x coordinate */
+	int y; /*y coordinate */
+	int p; /* pressure */
+	int flag; /* touch event flag: 0 -- down; 1-- up; 2 -- contact */
+	int id; /*touch ID */
+	int area;
+	int minor;
 };
 
 struct pen_event {
-    int down;
-    int inrange;
-    int tip;
-    int x;      /*x coordinate */
-    int y;      /*y coordinate */
-    int p;      /* pressure */
-    int flag;   /* touch event flag: 0 -- down; 1-- up; 2 -- contact */
-    int id;     /*touch ID */
-    int tilt_x;
-    int tilt_y;
-    int azimuth;
-    int tool_type;
+	int down;
+	int inrange;
+	int tip;
+	int x; /*x coordinate */
+	int y; /*y coordinate */
+	int p; /* pressure */
+	int flag; /* touch event flag: 0 -- down; 1-- up; 2 -- contact */
+	int id; /*touch ID */
+	int tilt_x;
+	int tilt_y;
+	int azimuth;
+	int tool_type;
+};
+struct fts_fod_info {
+	u8 fp_id;
+	u8 event_type;
+	u8 fp_area_rate;
+	u8 tp_area;
+	u16 fp_x;
+	u16 fp_y;
+	u8 fp_down;
+	u8 fp_down_report;
 };
 
 struct fts_ts_data {
-    struct i2c_client *client;
-    struct spi_device *spi;
-    struct device *dev;
-    struct input_dev *input_dev;
-    struct input_dev *pen_dev;
-    struct fts_ts_platform_data *pdata;
-    struct ts_ic_info ic_info;
-    struct workqueue_struct *ts_workqueue;
-    struct work_struct fwupg_work;
-    struct delayed_work esdcheck_work;
-    struct delayed_work prc_work;
-    struct work_struct resume_work;
-    wait_queue_head_t ts_waitqueue;
-    struct ftxxxx_proc proc;
-    struct ftxxxx_proc proc_ta;
-    spinlock_t irq_lock;
-    struct mutex report_mutex;
-    struct mutex bus_lock;
-    unsigned long intr_jiffies;
-    int irq;
-    int log_level;
-    int fw_is_running;      /* confirm fw is running when using spi:default 0 */
-    int dummy_byte;
+	struct i2c_client *client;
+	struct spi_device *spi;
+	struct device *dev;
+	struct input_dev *input_dev;
+	struct input_dev *pen_dev;
+	struct fts_ts_platform_data *pdata;
+	struct ts_ic_info ic_info;
+	struct workqueue_struct *ts_workqueue;
+	struct work_struct fwupg_work;
+	struct delayed_work esdcheck_work;
+	struct delayed_work prc_work;
+	struct work_struct resume_work;
+	wait_queue_head_t ts_waitqueue;
+	struct ftxxxx_proc proc;
+	struct ftxxxx_proc proc_ta;
+	struct ftxxxx_proc proc_charger;
+	spinlock_t irq_lock;
+	struct mutex report_mutex;
+	struct mutex bus_lock;
+	unsigned long intr_jiffies;
+	int irq;
+	int log_level;
+	int fw_is_running; /* confirm fw is running when using spi:default 0 */
+	int dummy_byte;
 #if IS_ENABLED(CONFIG_PM) && FTS_PATCH_COMERR_PM
-    struct completion pm_completion;
-    bool pm_suspend;
+	struct completion pm_completion;
+	bool pm_suspend;
 #endif
-    bool suspended;
-    bool fw_loading;
-    bool irq_disabled;
-    bool power_disabled;
-    bool glove_mode;
-    bool cover_mode;
-    bool charger_mode;
-    bool touch_analysis_support;
-    bool prc_support;
-    bool prc_mode;
-    bool esd_support;
-    /* N17 code for HQ-291087 by liunianliang at 2023/5/29 start */
-    bool earphone_mode;
-    u8 edge_mode;
-    /* N17 code for HQ-291087 by liunianliang at 2023/5/29 end */
-    bool gesture_support;   /* gesture enable or disable, default: disable */
-    u8 gesture_bmode;       /*gesture buffer mode*/
+	bool suspended;
+	bool fw_loading;
+	bool irq_disabled;
+	bool power_disabled;
+	bool glove_mode;
+	bool cover_mode;
+	bool charger_mode;
+	bool touch_analysis_support;
+	bool prc_support;
+	bool prc_mode;
+	bool esd_support;
+	bool pocket_mode;
+	bool game_mode;
+	bool gesture_support; /* gesture enable or disable, default: disable */
+	u8 gesture_bmode; /*gesture buffer mode*/
+	u8 fod_support; /* Fod enable or disable, default: disable */
+	u8 pen_etype;
+	struct pen_event pevent;
+	struct ts_event events[FTS_MAX_POINTS_SUPPORT]; /* multi-touch */
+	u8 touch_addr;
+	u32 touch_size;
+	u8 *touch_buf;
+	int touch_event_num;
+	int touch_points;
+	int key_state;
+	int ta_flag;
+	u32 ta_size;
+	u8 *ta_buf;
 
-    u8 pen_etype;
-    struct pen_event pevent;
-    struct ts_event events[FTS_MAX_POINTS_SUPPORT];    /* multi-touch */
-    u8 touch_addr;
-    u32 touch_size;
-    u8 *touch_buf;
-    int touch_event_num;
-    int touch_points;
-    int key_state;
-    int ta_flag;
-    u32 ta_size;
-    u8 *ta_buf;
-
-    u8 *bus_tx_buf;
-    u8 *bus_rx_buf;
-    int bus_type;
-    int bus_ver;
-    struct regulator *vdd;
-    struct regulator *iovdd;
-    struct regulator *vcc_i2c;
+	u8 *bus_tx_buf;
+	u8 *bus_rx_buf;
+	int bus_type;
+	int bus_ver;
+	struct regulator *vdd;
+	struct regulator *vcc_i2c;
+	struct fts_fod_info fod_info;
 #if FTS_PINCTRL_EN
-    struct pinctrl *pinctrl;
-    struct pinctrl_state *pins_active;
-    struct pinctrl_state *pins_suspend;
-    struct pinctrl_state *pins_release;
+	struct pinctrl *pinctrl;
+	struct pinctrl_state *pins_active;
+	struct pinctrl_state *pins_suspend;
+	struct pinctrl_state *pins_release;
 #endif
 
-    struct notifier_block fb_notif;
-    /* N17 code for HQ-291087 by liunianliang at 2023/5/29 start */
-    char vendor[32];
-    /* N17 code for HQ-291087 by liunianliang at 2023/5/29 end */
-
-    /* N17 code for HQ-291711 by liunianliang at 2023/06/03 start */
-    struct power_supply *usb_psy;
-    struct tcpc_device *tcpc;
-    struct notifier_block tcpc_nb;
-    struct notifier_block earphone_nb;
-    /* N17 code for HQ-291711 by liunianliang at 2023/06/03 end */
-
-    /* N17 code for HQ-290835 by liunianliang at 2023/6/12 start */
-    struct mutex cmd_update_mutex;
-    int palm_sensor_switch;
-    /* N17 code for HQ-290835 by liunianliang at 2023/6/12 end */
-
-    /* N17 code for HQ-299546 by liunianliang at 2023/6/13 start */
-    bool is_expert_mode;
-    bool gamemode_enabled;
-    u8 gesture_cmd;
-    u8 gesture_status;
-    int gesture_cmd_delay;
-    /* N17 code for HQ-299546 by liunianliang at 2023/6/13 end */
-
-    /* N17 code for HQ-299728 by liunianliang at 2023/6/15 start */
-    u8 lockdown_info[0x20];
-    /* N17 code for HQ-299728 by liunianliang at 2023/6/15 end */
+	struct notifier_block fb_notif;
 };
 
-/* N17 code for HQ-299546 by liunianliang at 2023/6/13 start */
-enum GESTURE_MODE_TYPE {
-    GESTURE_DOUBLETAP,
-    GESTURE_AOD,
-    GESTURE_FOD,
-};
-/* N17 code for HQ-299546 by liunianliang at 2023/6/13 end */
 
 enum _FTS_BUS_TYPE {
-    BUS_TYPE_NONE,
-    BUS_TYPE_I2C,
-    BUS_TYPE_SPI,
+	BUS_TYPE_NONE,
+	BUS_TYPE_I2C,
+	BUS_TYPE_SPI,
 };
 
 enum _FTS_BUS_VER {
-    BUS_VER_DEFAULT = 1,
-    BUS_VER_V2,
+	BUS_VER_DEFAULT = 1,
+	BUS_VER_V2,
 };
 
 enum _FTS_TOUCH_ETYPE {
-    TOUCH_DEFAULT = 0x00,
-    TOUCH_PROTOCOL_v2 = 0x02,
-    TOUCH_EXTRA_MSG = 0x08,
-    TOUCH_PEN = 0x0B,
-    TOUCH_GESTURE = 0x80,
-    TOUCH_FW_INIT = 0x81,
-    TOUCH_DEFAULT_HI_RES = 0x82,
-    TOUCH_IGNORE = 0xFE,
-    TOUCH_ERROR = 0xFF,
+	TOUCH_DEFAULT = 0x00,
+	TOUCH_PROTOCOL_v2 = 0x02,
+	TOUCH_EXTRA_MSG = 0x08,
+	TOUCH_PEN = 0x0B,
+	TOUCH_GESTURE = 0x80,
+	TOUCH_FW_INIT = 0x81,
+	TOUCH_DEFAULT_HI_RES = 0x82,
+	TOUCH_FOD = 0x83,
+	TOUCH_IGNORE = 0xFE,
+	TOUCH_ERROR = 0xFF,
 };
 
 enum _FTS_STYLUS_ETYPE {
-    STYLUS_DEFAULT,
-    STYLUS_HOVER,
+	STYLUS_DEFAULT,
+	STYLUS_HOVER,
 };
 
 enum _FTS_GESTURE_BMODE {
-    GESTURE_BM_REG,
-    GESTURE_BM_TOUCH,
+	GESTURE_BM_REG,
+	GESTURE_BM_TOUCH,
 };
-
-/* N17 code for HQ-291087 by liunianliang at 2023/5/29 start */
-struct lct_tp_data_dump {
-    u8 tx_num;
-    u8 rx_num;
-    int *rawdata;
-    int *diffdata;
-    struct proc_dir_entry *tp_data_dump_proc;
-};
-/* N17 code for HQ-291087 by liunianliang at 2023/5/29 end */
 
 /*****************************************************************************
 * Global variable or extern global variabls/functions
@@ -395,6 +358,7 @@ void fts_gesture_recovery(struct fts_ts_data *ts_data);
 int fts_gesture_readdata(struct fts_ts_data *ts_data, u8 *data);
 int fts_gesture_suspend(struct fts_ts_data *ts_data);
 int fts_gesture_resume(struct fts_ts_data *ts_data);
+void fts_fod_report_key(struct fts_ts_data *ts_data);
 
 /* Apk and functions */
 int fts_create_apk_debug_channel(struct fts_ts_data *);
@@ -403,17 +367,6 @@ void fts_release_apk_debug_channel(struct fts_ts_data *);
 /* ADB functions */
 int fts_create_sysfs(struct fts_ts_data *ts_data);
 int fts_remove_sysfs(struct fts_ts_data *ts_data);
-
-/* N17 code for HQ-291087 by liunianliang at 2023/5/29 start */
-/* cust functions */
-int fts_create_procfs(struct fts_ts_data *ts_data);
-int fts_remove_procfs(struct fts_ts_data *ts_data);
-extern const struct proc_ops fts_tp_data_dump_ops;
-extern const struct proc_ops fts_ito_test_ops;
-extern bool ito_test_result;
-extern u8 ito_test_status;
-extern struct lct_tp_data_dump *lct_tp_data_dump_p;
-/* N17 code for HQ-291087 by liunianliang at 2023/5/29 end */
 
 /* ESD */
 int fts_esdcheck_init(struct fts_ts_data *ts_data);
@@ -426,8 +379,6 @@ bool fts_esd_is_disable(void);
 
 
 /* Host test */
-int fts_test_init(struct fts_ts_data *ts_data);
-int fts_test_exit(struct fts_ts_data *ts_data);
 
 /* Point Report Check*/
 int fts_point_report_check_init(struct fts_ts_data *ts_data);
@@ -442,12 +393,10 @@ int fts_upgrade_bin(char *fw_name, bool force);
 int fts_enter_test_environment(bool test_state);
 int fts_enter_gesture_fw(void);
 int fts_enter_normal_fw(void);
-/* N17 code for HQ-291087 by liunianliang at 2023/5/29 start */
-int fts_read_lockdown_info(u8 *buf);
-/* N17 code for HQ-291087 by liunianliang at 2023/5/29 end */
+
 
 /* Other */
-int fts_reset_proc(int hdelayms);
+int fts_reset_proc(struct fts_ts_data *ts_data, int hdelayms);
 int fts_check_cid(struct fts_ts_data *ts_data, u8 id_h);
 int fts_wait_tp_to_valid(void);
 void fts_release_all_finger(void);
@@ -455,21 +404,22 @@ void fts_tp_state_recovery(struct fts_ts_data *ts_data);
 int fts_ex_mode_init(struct fts_ts_data *ts_data);
 int fts_ex_mode_exit(struct fts_ts_data *ts_data);
 int fts_ex_mode_recovery(struct fts_ts_data *ts_data);
+extern void fts_read_fod_info(struct fts_ts_data *ts_data);
+int fts_reset_proc(struct fts_ts_data *ts_data, int hdelayms);
 
 void fts_irq_disable(void);
 void fts_irq_enable(void);
+int fts_ts_suspend(struct device *dev);
 
-/* N17 code for HQ-299560 by zhangzhijian5 at 2023/8/16 start */
-int fts_reset_proc_extend(int hdelayms);
-/* N17 code for HQ-299560 by zhangzhijian5 at 2023/8/16 end */
+#if FTS_PSENSOR_EN
+int fts_proximity_init(void);
+int fts_proximity_exit(void);
+int fts_proximity_readdata(struct fts_ts_data *ts_data);
+int fts_proximity_suspend(void);
+int fts_proximity_resume(void);
+int fts_proximity_recovery(struct fts_ts_data *ts_data);
+#endif
 
-/* N17 code for HQ-306279 by liunianliang at 2023/07/07 start */
-extern int fts_init_lockdown_info(u8 *buf);
-extern int fts_ts_hw_info(struct fts_ts_data *ts_data);
-/* N17 code for HQ-306279 by liunianliang at 2023/07/07 end */
-
-/* N17 code for HQ-310974 by xionglei6 at 2023/08/14 start */
-extern const char* tp_vendor;
-/* N17 code for HQ-310974 by xionglei6 at 2023/08/14 end */
+extern int tp_gesture_flag;
 
 #endif /* __LINUX_FOCALTECH_CORE_H__ */
--- drivers/input/touchscreen/FT3683G/focaltech_point_report_check.c
+++ drivers/input/touchscreen/FT3683G/focaltech_point_report_check.c
@@ -39,8 +39,8 @@
 /*****************************************************************************
 * Private constant and macro definitions using #define
 *****************************************************************************/
-#define POINT_REPORT_CHECK_WAIT_TIME                200    /* unit:ms */
-#define PRC_INTR_INTERVALS                          100    /* unit:ms */
+#define POINT_REPORT_CHECK_WAIT_TIME			200 /* unit:ms */
+#define PRC_INTR_INTERVALS				100 /* unit:ms */
 
 /*****************************************************************************
 * Static variables
@@ -58,29 +58,29 @@
 *****************************************************************************/
 static void fts_prc_func(struct work_struct *work)
 {
-    struct fts_ts_data *ts_data = container_of(work,
-                                  struct fts_ts_data, prc_work.work);
-    unsigned long cur_jiffies = jiffies;
-    unsigned long intr_timeout = msecs_to_jiffies(PRC_INTR_INTERVALS);
+	struct fts_ts_data *ts_data = container_of(work,
+					struct fts_ts_data, prc_work.work);
+	unsigned long cur_jiffies = jiffies;
+	unsigned long intr_timeout = msecs_to_jiffies(PRC_INTR_INTERVALS);
 
-    if (ts_data->prc_support && !ts_data->suspended) {
-        intr_timeout += ts_data->intr_jiffies;
-        if (time_after(cur_jiffies, intr_timeout)) {
-            if (ts_data->touch_points) {
-                fts_release_all_finger();
-                if (ts_data->log_level >= 3)
-                    FTS_DEBUG("prc trigger interval:%dms",
-                              jiffies_to_msecs(cur_jiffies - ts_data->intr_jiffies));
-            }
-            ts_data->prc_mode = 0;
-        } else {
-            queue_delayed_work(ts_data->ts_workqueue, &ts_data->prc_work,
-                               msecs_to_jiffies(POINT_REPORT_CHECK_WAIT_TIME));
-            ts_data->prc_mode = 1;
-        }
-    } else {
-        ts_data->prc_mode = 0;
-    }
+	if (ts_data->prc_support && !ts_data->suspended) {
+		intr_timeout += ts_data->intr_jiffies;
+		if (time_after(cur_jiffies, intr_timeout)) {
+			if (ts_data->touch_points) {
+				fts_release_all_finger();
+				if (ts_data->log_level >= 3)
+					FTS_DEBUG("prc trigger interval:%dms",
+						jiffies_to_msecs(cur_jiffies - ts_data->intr_jiffies));
+			}
+			ts_data->prc_mode = 0;
+		} else {
+			queue_delayed_work(ts_data->ts_workqueue, &ts_data->prc_work,
+					msecs_to_jiffies(POINT_REPORT_CHECK_WAIT_TIME));
+			ts_data->prc_mode = 1;
+		}
+	} else {
+		ts_data->prc_mode = 0;
+	}
 }
 
 /*****************************************************************************
@@ -92,48 +92,48 @@ static void fts_prc_func(struct work_struct *work)
 *****************************************************************************/
 void fts_prc_queue_work(struct fts_ts_data *ts_data)
 {
-    if (ts_data->prc_support && !ts_data->prc_mode && !ts_data->suspended) {
-        queue_delayed_work(ts_data->ts_workqueue, &ts_data->prc_work,
-                           msecs_to_jiffies(POINT_REPORT_CHECK_WAIT_TIME));
-        ts_data->prc_mode = 1;
-    }
+	if (ts_data->prc_support && !ts_data->prc_mode && !ts_data->suspended) {
+		queue_delayed_work(ts_data->ts_workqueue, &ts_data->prc_work,
+				msecs_to_jiffies(POINT_REPORT_CHECK_WAIT_TIME));
+		ts_data->prc_mode = 1;
+	}
 }
 
 
 static ssize_t fts_prc_store(
-    struct device *dev,
-    struct device_attribute *attr, const char *buf, size_t count)
+	struct device *dev,
+	struct device_attribute *attr, const char *buf, size_t count)
 {
-    struct fts_ts_data *ts_data = dev_get_drvdata(dev);
-    struct input_dev *input_dev = ts_data->input_dev;
+	struct fts_ts_data *ts_data = dev_get_drvdata(dev);
+	struct input_dev *input_dev = ts_data->input_dev;
 
-    mutex_lock(&input_dev->mutex);
-    if (FTS_SYSFS_ECHO_ON(buf)) {
-        FTS_DEBUG("enable prc");
-        ts_data->prc_support = ENABLE;
-    } else if (FTS_SYSFS_ECHO_OFF(buf)) {
-        FTS_DEBUG("disable prc");
-        cancel_delayed_work_sync(&ts_data->prc_work);
-        ts_data->prc_support = DISABLE;
-    }
-    mutex_unlock(&input_dev->mutex);
+	mutex_lock(&input_dev->mutex);
+	if (FTS_SYSFS_ECHO_ON(buf)) {
+		FTS_DEBUG("enable prc");
+		ts_data->prc_support = ENABLE;
+	} else if (FTS_SYSFS_ECHO_OFF(buf)) {
+		FTS_DEBUG("disable prc");
+		cancel_delayed_work_sync(&ts_data->prc_work);
+		ts_data->prc_support = DISABLE;
+	}
+	mutex_unlock(&input_dev->mutex);
 
-    return count;
+	return count;
 }
 
 static ssize_t fts_prc_show(
-    struct device *dev, struct device_attribute *attr, char *buf)
+	struct device *dev, struct device_attribute *attr, char *buf)
 {
-    int count;
-    struct fts_ts_data *ts_data = dev_get_drvdata(dev);
-    struct input_dev *input_dev = ts_data->input_dev;
+	int count;
+	struct fts_ts_data *ts_data = dev_get_drvdata(dev);
+	struct input_dev *input_dev = ts_data->input_dev;
 
-    mutex_lock(&input_dev->mutex);
-    count = snprintf(buf, PAGE_SIZE, "PRC: %s\n", \
-                     ts_data->prc_support ? "Enable" : "Disable");
-    mutex_unlock(&input_dev->mutex);
+	mutex_lock(&input_dev->mutex);
+	count = snprintf(buf, PAGE_SIZE, "PRC: %s\n", \
+			ts_data->prc_support ? "Enable" : "Disable");
+	mutex_unlock(&input_dev->mutex);
 
-    return count;
+	return count;
 }
 
 static DEVICE_ATTR(fts_prc, S_IRUGO | S_IWUSR, fts_prc_show, fts_prc_store);
@@ -147,25 +147,25 @@ static DEVICE_ATTR(fts_prc, S_IRUGO | S_IWUSR, fts_prc_show, fts_prc_store);
 *****************************************************************************/
 int fts_point_report_check_init(struct fts_ts_data *ts_data)
 {
-    int ret = 0;
+	int ret = 0;
 
-    FTS_FUNC_ENTER();
+	FTS_FUNC_ENTER();
 
-    if (ts_data->ts_workqueue) {
-        INIT_DELAYED_WORK(&ts_data->prc_work, fts_prc_func);
-    } else {
-        FTS_ERROR("fts workqueue is NULL, can't run point report check function");
-        return -EINVAL;
-    }
+	if (ts_data->ts_workqueue) {
+		INIT_DELAYED_WORK(&ts_data->prc_work, fts_prc_func);
+	} else {
+		FTS_ERROR("fts workqueue is NULL, can't run point report check function");
+		return -EINVAL;
+	}
 
-    ret = sysfs_create_file(&ts_data->dev->kobj, &dev_attr_fts_prc.attr);
-    if ( ret < 0) {
-        FTS_ERROR("create prc sysfs fail");
-    }
+	ret = sysfs_create_file(&ts_data->dev->kobj, &dev_attr_fts_prc.attr);
+	if ( ret < 0) {
+		FTS_ERROR("create prc sysfs fail");
+	}
 
-    ts_data->prc_support = FTS_POINT_REPORT_CHECK_EN;
-    FTS_FUNC_EXIT();
-    return 0;
+	ts_data->prc_support = FTS_POINT_REPORT_CHECK_EN;
+	FTS_FUNC_EXIT();
+	return 0;
 }
 
 /*****************************************************************************
@@ -177,9 +177,9 @@ int fts_point_report_check_init(struct fts_ts_data *ts_data)
 *****************************************************************************/
 int fts_point_report_check_exit(struct fts_ts_data *ts_data)
 {
-    FTS_FUNC_ENTER();
-    cancel_delayed_work_sync(&ts_data->prc_work);
-    sysfs_remove_file(&ts_data->dev->kobj, &dev_attr_fts_prc.attr);
-    FTS_FUNC_EXIT();
-    return 0;
+	FTS_FUNC_ENTER();
+	cancel_delayed_work_sync(&ts_data->prc_work);
+	sysfs_remove_file(&ts_data->dev->kobj, &dev_attr_fts_prc.attr);
+	FTS_FUNC_EXIT();
+	return 0;
 }
