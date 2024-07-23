d23750049a04712e19e2c8f62ad2bdae309691f7
Elias Dimopoulos <linkdevel@protonmail.com>
2024-07-24 00:54:03 +0300

    diff with qcom fts driver
    Signed-off-by: Elias Dimopoulos <linkdevel@protonmail.com>

--- drivers/input/touchscreen/FT3683G/focaltech_common.h
+++ drivers/input/touchscreen/FT3683G/focaltech_common.h
@@ -2,7 +2,7 @@
  *
  * FocalTech fts TouchScreen driver.
  *
- * Copyright (c) 2012-2020, Focaltech Ltd. All rights reserved.
+ * Copyright (c) 2012-2019, Focaltech Ltd. All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -36,7 +36,7 @@
 /*****************************************************************************
 * Macro definitions using #define
 *****************************************************************************/
-#define FTS_DRIVER_VERSION                  "FocalTech V4.1 20230424"
+#define FTS_DRIVER_VERSION      "Focaltech V3.2 20200422"
 
 #define BYTE_OFF_0(x)           (u8)((x) & 0xFF)
 #define BYTE_OFF_8(x)           (u8)(((x) >> 8) & 0xFF)
@@ -49,16 +49,10 @@
 #define FLAG_HID_BIT            10
 #define FLAG_IDC_BIT            11
 
-#define IC_SERIALS              (FTS_CHIP_TYPE & FLAGBITS(0, FLAG_ICSERIALS_LEN-1))
+#define IC_SERIALS(type)        ((type) & FLAGBITS(0, FLAG_ICSERIALS_LEN-1))
 #define IC_TO_SERIALS(x)        ((x) & FLAGBITS(0, FLAG_ICSERIALS_LEN-1))
-#define FTS_CHIP_IDC            ((FTS_CHIP_TYPE & FLAGBIT(FLAG_IDC_BIT)) == FLAGBIT(FLAG_IDC_BIT))
-#define FTS_HID_SUPPORTTED      ((FTS_CHIP_TYPE & FLAGBIT(FLAG_HID_BIT)) == FLAGBIT(FLAG_HID_BIT))
-
-#define FTS_MAX_CHIP_IDS        8
-
-/* N17 code for HQ-291087 by liunianliang at 2023/5/29 start */
-#define FTS_CHIP_TYPE_MAPPING {{0x90, 0x56, 0x72, 0x00, 0x00, 0x00, 0x00, 0x36, 0xB3}}
-/* N17 code for HQ-291087 by liunianliang at 2023/5/29 end */
+#define FTS_CHIP_IDC(type)            (((type) & FLAGBIT(FLAG_IDC_BIT)) == FLAGBIT(FLAG_IDC_BIT))
+#define FTS_HID_SUPPORTTED(type)      (((type) & FLAGBIT(FLAG_HID_BIT)) == FLAGBIT(FLAG_HID_BIT))
 
 #define FILE_NAME_LENGTH                    128
 #define ENABLE                              1
@@ -71,8 +65,7 @@
 #define FTS_CMD_READ_ID                     0x90
 #define FTS_CMD_READ_ID_LEN                 4
 #define FTS_CMD_READ_ID_LEN_INCELL          1
-#define FTS_CMD_READ_INFO                   0x5E
-
+#define FTS_CMD_READ_FW_CONF                0xA8
 /*register address*/
 #define FTS_REG_INT_CNT                     0x8F
 #define FTS_REG_FLOW_WORK_CNT               0x91
@@ -92,58 +85,29 @@
 #define FTS_REG_FACE_DEC_MODE_STATUS        0x01
 #define FTS_REG_IDE_PARA_VER_ID             0xB5
 #define FTS_REG_IDE_PARA_STATUS             0xB6
-/* N17 code for HQ-299560 by zhangzhijian5 at 2023/8/16 start */
-#define FTS_REG_IDE_PARA_STATUS_EN          0x01
-/* N17 code for HQ-299560 by zhangzhijian5 at 2023/8/16 end */
 #define FTS_REG_GLOVE_MODE_EN               0xC0
 #define FTS_REG_COVER_MODE_EN               0xC1
+#define FTS_REG_REPORT_RATE                 0x88
 #define FTS_REG_CHARGER_MODE_EN             0x8B
-/* N17 code for HQ-291087 by liunianliang at 2023/5/29 start */
-#define FTS_REG_EARPHONE_MODE_EN            0xC3
-#define FTS_REG_EDGE_MODE_EN                0x8C // USB ports Right: 1, USB ports Left: 2
-/* N17 code for HQ-291087 by liunianliang at 2023/5/29 end */
 #define FTS_REG_GESTURE_EN                  0xD0
 #define FTS_REG_GESTURE_OUTPUT_ADDRESS      0xD3
 #define FTS_REG_MODULE_ID                   0xE3
 #define FTS_REG_LIC_VER                     0xE4
 #define FTS_REG_ESD_SATURATE                0xED
 
-/* N17 code for HQ-290835 by liunianliang at 2023/6/12 start */
-#define FTS_PALM_EN                         0x9a
-#define FTS_PALM_DATA                       0x9b
-#define FTS_PALM_ON                         0x05
-#define FTS_PALM_OFF                        0x00
-#define FTS_PALM_KEYCODE                    523
-/* N17 code for HQ-290835 by liunianliang at 2023/6/12 end */
-
-/* N17 code for HQ-299546 by liunianliang at 2023/6/13 start */
-#define FTS_REG_EDGE_FILTER_EN              0x8c
-#define FTS_REG_EDGE_FILTER_LEVEL           0x8d
-#define FTS_REG_GAMEMODE                    0xC1
-#define FTS_REG_ORIENTATION                 0x8C
-#define FTS_GESTURE_CTRL                    0xD1
-#define FTS_GESTURE_DOUBLETAP               0x04
-#define FTS_GESTURE_AOD                     0x07
-/* N17 code for HQ-299546 by liunianliang at 2023/6/13 end */
-
-/* N17 code for HQ-310258 by zhangzhijian5 at 2023/7/29 start */
-#define FTS_REG_HDLEMODE                    0x8E
-/* N17 code for HQ-310258 by zhangzhijian5 at 2023/7/29 end */
-
-/* N17 code for HQ-291087 by liunianliang at 2023/5/29 start */
-#define FTS_SYSFS_ECHO_ON(buf)      (buf[0] == '1' || buf[0] == '2')
-/* N17 code for HQ-291087 by liunianliang at 2023/5/29 end */
+#define FTS_SYSFS_ECHO_ON(buf)      (buf[0] == '1')
 #define FTS_SYSFS_ECHO_OFF(buf)     (buf[0] == '0')
 
 #define kfree_safe(pbuf) do {\
-    if (pbuf) {\
-        kfree(pbuf);\
-        pbuf = NULL;\
-    }\
+	if (pbuf) {\
+		kfree(pbuf);\
+		pbuf = NULL;\
+	}\
 } while(0)
 
 /*****************************************************************************
-*  Alternative mode (When something goes wrong, the modules may be able to solve the problem.)
+*  Alternative mode (When something goes wrong,
+* the modules may be able to solve the problem.)
 *****************************************************************************/
 /*
  * point report check
@@ -155,27 +119,21 @@
 * Global variable or extern global variabls/functions
 *****************************************************************************/
 struct ft_chip_t {
-    u16 type;
-    u8 chip_idh;
-    u8 chip_idl;
-    u8 rom_idh;
-    u8 rom_idl;
-    u8 pb_idh;
-    u8 pb_idl;
-    u8 bl_idh;
-    u8 bl_idl;
-};
-
-struct ft_chip_id_t {
-    u16 type;
-    u16 chip_ids[FTS_MAX_CHIP_IDS];
+	u64 type;
+	u8 chip_idh;
+	u8 chip_idl;
+	u8 rom_idh;
+	u8 rom_idl;
+	u8 pb_idh;
+	u8 pb_idl;
+	u8 bl_idh;
+	u8 bl_idl;
 };
 
 struct ts_ic_info {
-    bool is_incell;
-    bool hid_supported;
-    struct ft_chip_t ids;
-    struct ft_chip_id_t cid;
+	bool is_incell;
+	bool hid_supported;
+	struct ft_chip_t ids;
 };
 
 /*****************************************************************************
@@ -183,15 +141,15 @@ struct ts_ic_info {
 *****************************************************************************/
 #if FTS_DEBUG_EN
 #define FTS_DEBUG(fmt, args...) do { \
-    printk("[FTS_TS]%s:"fmt"\n", __func__, ##args); \
+	printk("[FTS_TS]%s:"fmt"\n", __func__, ##args); \
 } while (0)
 
 #define FTS_FUNC_ENTER() do { \
-    printk("[FTS_TS]%s: Enter\n", __func__); \
+	printk("[FTS_TS]%s: Enter\n", __func__); \
 } while (0)
 
 #define FTS_FUNC_EXIT() do { \
-    printk("[FTS_TS]%s: Exit(%d)\n", __func__, __LINE__); \
+	printk("[FTS_TS]%s: Exit(%d)\n", __func__, __LINE__); \
 } while (0)
 #else /* #if FTS_DEBUG_EN*/
 #define FTS_DEBUG(fmt, args...)
@@ -200,10 +158,10 @@ struct ts_ic_info {
 #endif
 
 #define FTS_INFO(fmt, args...) do { \
-    printk(KERN_INFO "[FTS_TS/I]%s:"fmt"\n", __func__, ##args); \
+	printk(KERN_INFO "[FTS_TS/I]%s:"fmt"\n", __func__, ##args); \
 } while (0)
 
 #define FTS_ERROR(fmt, args...) do { \
-    printk(KERN_ERR "[FTS_TS/E]%s:"fmt"\n", __func__, ##args); \
+	printk(KERN_ERR "[FTS_TS/E]%s:"fmt"\n", __func__, ##args); \
 } while (0)
 #endif /* __LINUX_FOCALTECH_COMMON_H__ */
